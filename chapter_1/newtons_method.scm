01a7f6cd235ec4587727baded6a3bf63e79a17f5
Hoon Wee <mrgravity817@gmail.com>
2023-11-01 23:31:12 +0900

    feat: sqrt with newton's method

---
+++ chapter_1/newtons_method.scm
@@ -0,0 +1,23 @@
+; Guess   Quotient       Average
+; 1       2/1 = 2        (2+1)/2 = 1.5
+; 1.5     2/1.5 = 1.33   (1.33+1.5)/2 = 1.4167
+; ...
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x)) ;; guess starts with 1
+
+;; Recursively process the guess until it's good enough
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x) 
+    guess 
+    (sqrt-iter (improve guess x) x)))
+
+;; See if guess^2 is almost equal to original number 
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
